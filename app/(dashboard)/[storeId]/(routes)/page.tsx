c4c0e278adaba833d5fa3ddb529dff9b6467857d
lucas kissmann <lucaskissmann@gmail.com>
2023-10-07 21:41:00 -0300

    wip

---
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+    return (
+        <div>
+            This is a Dash
+        </div>
+    );
+}
+
+export default DashboardPage;
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
+import { useEffect } from "react";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 
 const SetupPage = () => {
   const onOpen = useStoreModal((state) => state.onOpen);  
-  const isOpen = useStoreModal((state) => state.onOpen);  
+  const isOpen = useStoreModal((state) => state.isOpen);  
+
+  useEffect(() => {
+    if( !isOpen ) {
+      onOpen();
+    }
+
+  }, [isOpen, onOpen])
   
   return (
       <div className="p-4">
