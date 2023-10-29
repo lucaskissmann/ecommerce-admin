7e076506c60a349b51a594ed98199004ee2b6aa3
lucas kissmann <lucaskissmann@gmail.com>
2023-10-29 11:01:28 -0300

    feat: add order entity

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+export type OrderColumn = {
+  id: string
+  phone: string
+  address: string
+  isPaid: boolean
+  totalPrice: string
+  products: string
+  createdAt: string
+}
+
+export const columns: ColumnDef<OrderColumn>[] = [
+  {
+    accessorKey: "products",
+    header: "Produtos",
+  },
+  {
+    accessorKey: "phone",
+    header: "Fone",
+  },
+  {
+    accessorKey: "address",
+    header: "Endereço",
+  },
+  {
+    accessorKey: "totalPrice",
+    header: "Preço final",
+  },
+  {
+    accessorKey: "isPaid",
+    header: "Pago",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Data de criação",
+  }
+]
